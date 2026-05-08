8ad608ddf1098798ec5a2deed35881d39057d45a
Tim De Graeve <codemeteor@gmail.com>
2026-05-08 07:45:42 +0200

    🐛 fix(app): read account_id and api_token form the settings file

--- src/cloudflare_dyndns_updater/settings.py
+++ src/cloudflare_dyndns_updater/settings.py
@@ -1,15 +1,18 @@
 from dataclasses import dataclass
-from typing import Dict
 from pathlib import Path
 from os import getenv
-import yaml
 import logging
 import sys
 import tomllib
 import pathlib
 
 
-SETTINGS_FILE = Path(getenv("SETTINGS_FILE", pathlib.Path.home() / ".config/cloudflare-dyndns-updater/config.toml"))
+SETTINGS_FILE = Path(
+    getenv(
+        "SETTINGS_FILE",
+        pathlib.Path.home() / ".config/cloudflare-dyndns-updater/config.toml",
+    )
+)
 
 
 @dataclass
@@ -18,8 +21,8 @@ class Settings:
     Settings class
     """
 
-    auth_email: str
-    auth_key: str
+    account_id: str
+    api_token: str
     zones: list
 
     def __init__(self) -> None:
@@ -28,15 +31,15 @@ class Settings:
         """
         self.read_settings()
 
-    def read_settings(self) -> Dict:
+    def read_settings(self) -> dict | None:
         """
         Read the settings from the config.toml file
         """
         try:
             with open(SETTINGS_FILE, "rb") as settings_file:
-                settings: Dict = tomllib.load(settings_file)
-            self.auth_email = settings["auth_email"]
-            self.auth_key = settings["auth_key"]
+                settings: dict = tomllib.load(settings_file)
+            self.account_id = settings["account_id"]
+            self.api_token = settings["api_token"]
             self.zones = settings["zone_name"]
 
         except Exception:
